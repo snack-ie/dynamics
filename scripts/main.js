f9137abc9d21cc66e25de5c387efbab8b20f129e
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-05-28 21:59:28 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
-var ost = loadMusic("mytime");
-require("unit");
-Events.on(WorldLoadEvent, e => {
-  Vars.control.sound.stop();
-  ost.stop();
-  ost.play();
-});
-if(!Vars.headless){
+f(!Vars.headless){
   Core.app.post(() => {
-    Vars.mods.locateMod("depression").meta.displayName = "[#333333]depression";
+    Vars.mods.locateMod("dynamics").meta.displayName = "[#333333]dynamics";
   });
 };
